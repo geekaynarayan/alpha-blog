c85f89c1d74a4adcd8f00df737bd38c3a8761794
Rails Simple Blog <gopal.narayan@live.com>
2018-05-22 01:01:31 -0700

    CRU out of CRUD is code complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 		@article = Article.new
 	end
@@ -12,9 +16,29 @@ class ArticlesController < ApplicationController
 		# render plain: params[:article].inspect
 
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_path(@article)
+		if(@article.save)
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if(@article.update(article_params))
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+
 
 	private def article_params
 		params.require(:article).permit(:title, :description)
